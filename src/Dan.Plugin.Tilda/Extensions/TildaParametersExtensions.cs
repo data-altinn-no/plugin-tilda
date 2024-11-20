52783208c51810758ecdebfac6b1f32d42466b5c
SondreJDigdir <sondre.jahrsengene@digdir.no>
2024-11-20 14:55:57 +0100

    fix: add step for specifically removing not found orgs if geo search is on (#16)

---
+++ src/Dan.Plugin.Tilda/Extensions/TildaParametersExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Dan.Plugin.Tilda.Models;
+
+namespace Dan.Plugin.Tilda.Extensions;
+
+public static class TildaParametersExtensions
+{
+    public static bool HasGeoSearchParams(this TildaParameters parameters)
+    {
+        if (parameters is null)
+        {
+            return false;
+        }
+
+        return !string.IsNullOrEmpty(parameters.postcode) ||
+               !string.IsNullOrEmpty(parameters.municipalityNumber) ||
+               !string.IsNullOrEmpty(parameters.nace);
+    }
+}
