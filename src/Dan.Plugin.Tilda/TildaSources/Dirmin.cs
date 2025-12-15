72fd2d57e37c994e319eca513c477f5def8b8568
SondreJDigdir <sondre.jahrsengene@digdir.no>
2025-12-15 13:18:39 +0100

    feat: add dirmin as tilda source (#56)

---
+++ src/Dan.Plugin.Tilda/TildaSources/Dirmin.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http;
+using Dan.Plugin.Tilda.Config;
+using Dan.Plugin.Tilda.Interfaces;
+using Dan.Plugin.Tilda.Utils;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Polly.Registry;
+
+namespace Dan.Plugin.Tilda.TildaSources;
+
+public class Dirmin(
+    IOptions<Settings> settings,
+    IHttpClientFactory httpClientFactory,
+    ILoggerFactory loggerFactory,
+    ResiliencePipelineProvider<string> pipelineProvider,
+    IUriFormatter uriFormatter)
+    :   TildaDataSource(settings, httpClientFactory, loggerFactory, pipelineProvider, uriFormatter),
+        ITildaAuditReports,
+        ITildaAuditCoordination,
+        ITildaAlertMessage
+{
+    public override string OrganizationNumber  => "974760282";
+    public override string ControlAgency => "Direktoratet for mineralforvaltning med Bergmesteren for Svalbard";
+}
